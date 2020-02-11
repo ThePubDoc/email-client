11c304d760f0374f7c649b2edbe4ec164e73027e
annubv <annubv25@gmail.com>
2020-02-11 23:34:03 +0530

    created completed campaign route, ctrl

--- backend/controller/mainController.js
+++ backend/controller/mainController.js
@@ -1,67 +1,74 @@
-const Campaign = require('../models/campaign')
-const multer = require('multer');
+const Campaign = require("../models/campaign");
+const multer = require("multer");
 
-async function index(req,res){
-    const campaigns = await Campaign.find({});
-    res.render("index",{
-        campaigns
-    });
+async function index(req, res) {
+  const campaigns = await Campaign.find({});
+  res.render("index", {
+    campaigns
+  });
 }
 
-function campaign(req,res){
-    res.render("campaign");
+async function completed_campaigns(req, res) {
+  const campaigns = await Campaign.find({});
+  res.render("completed-campaigns", {
+    campaigns
+  });
 }
 
-function edit(req,res){
-    const id = req.query.id;
-    
-    Campaign.find({_id : id})
-        .exec(function(err,campaign){
-            if(err){
-                console.log("Error in fetching id")
-            }
-            else{
-                res.render("edit", {
-                    campaign : campaign
-                })
-            }
-        })
+function campaign(req, res) {
+  res.render("campaign");
 }
 
-function del(req,res){
-    const id = req.query.id;
-    Campaign.findOneAndDelete({_id : id})
-        .exec((err,campaign)=>{
-            if(err){
-                console.log("Error in deleting");
-            }
-            else{
-                res.redirect('/')
-            }
-        })
+function edit(req, res) {
+  const id = req.query.id;
+
+  Campaign.find({ _id: id }).exec(function(err, campaign) {
+    if (err) {
+      console.log("Error in fetching id");
+    } else {
+      res.render("edit", {
+        campaign: campaign
+      });
+    }
+  });
 }
 
-function copy(req,res){
-    const id = req.query.id;
-    console.log(id);
-    Campaign.findOne({_id : id})
-      .exec((err,doc)=>{
-        if(err){
-          console.log("Error in fetching in cpoy")
-        }
-        else{
-          const {name, format, subject, file_url} = doc
-          const campaign_instance = new Campaign({name, format, subject, file_url})
-          campaign_instance.save();
-          res.redirect("/");
-        }
-      })
+function del(req, res) {
+  const id = req.query.id;
+  Campaign.findOneAndDelete({ _id: id }).exec((err, campaign) => {
+    if (err) {
+      console.log("Error in deleting");
+    } else {
+      res.redirect("/");
+    }
+  });
+}
+
+function copy(req, res) {
+  const id = req.query.id;
+  console.log(id);
+  Campaign.findOne({ _id: id }).exec((err, doc) => {
+    if (err) {
+      console.log("Error in fetching in cpoy");
+    } else {
+      const { name, format, subject, file_url } = doc;
+      const campaign_instance = new Campaign({
+        name,
+        format,
+        subject,
+        file_url
+      });
+      campaign_instance.save();
+      res.redirect("/");
+    }
+  });
 }
 
 module.exports = {
-    index : index,
-    campaign : campaign,
-    edit : edit,
-    del : del,
-    copy : copy,
-}
+  index,
+  campaign,
+  edit,
+  del,
+  copy,
+  completed_campaigns
+};
--- backend/routes/mainRoutes.js
+++ backend/routes/mainRoutes.js
@@ -1,31 +1,31 @@
-const express = require('express');
+const express = require("express");
 
-const mainController = require('../controller/mainController');
-const campaignController = require('../controller/campaignController');
+const mainController = require("../controller/mainController");
+const campaignController = require("../controller/campaignController");
 
 const router = express.Router();
 const app = express();
 const multer = require("multer");
 
 var storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, './uploads')
-    },
-    filename: function (req, file, cb) {
-      cb(null, file.originalname)
-    }
+  destination: function(req, file, cb) {
+    cb(null, "./uploads");
+  },
+  filename: function(req, file, cb) {
+    cb(null, file.originalname);
+  }
 });
-   
+
 var upload = multer({ storage: storage });
 
-router.route('/').get(mainController.index);
-router.route('/campaign').get(mainController.campaign);
-router.route('/edit').get(mainController.edit);
-router.route('/delete').get(mainController.del);
-router.route('/copy').get(mainController.copy);
+router.route("/").get(mainController.index);
+router.route("/campaign").get(mainController.campaign);
+router.route("/edit").get(mainController.edit);
+router.route("/delete").get(mainController.del);
+router.route("/copy").get(mainController.copy);
+router.route("/campaigns/completed").get(mainController.completed_campaigns);
 
-router.route("/campaign").post(upload.single('file'), campaignController.index);
-router.route("/edit").post(upload.single('file'), campaignController.edit);
-
+router.route("/campaign").post(upload.single("file"), campaignController.index);
+router.route("/edit").post(upload.single("file"), campaignController.edit);
 
 module.exports = router;
