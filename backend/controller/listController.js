d2e2612f8e313da7118eee3b770307d5bc5b6265
aayush <aayush9152@gmail.com>
2020-02-12 17:17:18 +0530

    fixed minor issues

--- backend/controller/listController.js
+++ backend/controller/listController.js
@@ -32,19 +32,30 @@ function createList(req, res) {
 
 function edit_list(req, res) {
   const id = req.query.id;
+  const file_url = req.file.path;
   const { name } = req.body;
-  List.findOneAndUpdate(
-    { _id: id },
-    { name },
-    { upsert: false },
-    (err, doc) => {
-      if (err) {
-        console.log("error in updating");
-      } else {
-        res.redirect("/lists");
-      }
-    }
-  );
+  let list_instance = new List({ name, file_url });
+  const new_file_url = path.join(__dirname ,"../../" , list_instance.file_url);
+  const results = [];
+  console.log(id)
+  fs.createReadStream(new_file_url)
+      .pipe(csv())
+      .on('data', (data) => results.push(data))
+      .on('end', () => {
+        const contacts = results.length;
+        List.findOneAndUpdate(
+          { _id: id },
+          { name, file_url, contacts },
+          { upsert: false },
+          (err, doc) => {
+            if (err) {
+              console.log("error in updating");
+            } else {
+              res.redirect("/lists");
+            }
+          }
+        );
+      });
 }
 
 module.exports = {
