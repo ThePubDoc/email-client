3ef536a7c24037e174904bdcf8ba66d26d68daf3
aayush <aayush9152@gmail.com>
2020-02-13 15:44:00 +0530

    aws

---
+++ ses1.js
@@ -0,0 +1,40 @@
+var AWS = require("aws-sdk");
+
+AWS.config.loadFromPath(__dirname + '/aws.json');
+
+var ses_mail = "From: <" + "aayush9152@gmail.com" + ">\n";
+    ses_mail = ses_mail + "To: " + "aayushsas@gmail.com" + "\n";
+    ses_mail = ses_mail + "Subject: AWS SES Attachment Example\n";
+    ses_mail = ses_mail + "MIME-Version: 1.0\n";
+    ses_mail = ses_mail + "Content-Type: multipart/mixed; boundary=\"NextPart\"\n\n";
+    ses_mail = ses_mail + "--NextPart\n";
+    ses_mail = ses_mail + "Content-Type: text/html; charset=us-ascii\n\n";
+    ses_mail = ses_mail + "This is the body of the email.\n\n";
+    ses_mail = ses_mail + "--NextPart\n";
+    ses_mail = ses_mail + "--NextPart--";
+    
+    var params = {
+        RawMessage: { Data: new Buffer(ses_mail) },
+        Destinations: [ "aayush9152@gmail.com" ],
+        Source: "<" + "aayush9152@gmail.com" + ">'"
+    };
+    
+    // ses.sendRawEmail(params, function(err, data) {
+    //     if(err) {
+    //         res.send(err);
+    //     } 
+    //     else {
+    //         res.send(data);
+    //     }           
+    // });
+
+  var sendPromise = new AWS.SES({apiVersion: '2010-12-01'}).sendRawEmail(params).promise();
+
+// Handle promise's fulfilled/rejected states
+sendPromise.then(
+  function(data) {
+    console.log(data.MessageId);
+  }).catch(
+    function(err) {
+    console.error(err, err.stack);
+  });
--- ses_listidentities.js
+++ ses_listidentities.js
@@ -1,37 +1,14 @@
-// Load the AWS SDK for Node.js
 var AWS = require('aws-sdk');
-// Set the region 
-// const credentials = require("./aws.json");
-
-// AWS.config.update = credentials;
-// AWS.config.update({region : "ap-south-1"});
-
-AWS.config.loadFromPath('./aws.json');
-// console.log(credentials)
 
 
-var params = {
-  Template: { 
-    TemplateName: 'test1', /* required */
-    HtmlPart: '<!DOCTYPE html>'+
-              '<html>'+
-              '<body>'+
-              '<h1>This text should be large, because it is formatted as a header in HTML.</h1>'+
-              '<p>Here is a formatted link: <a href="https://docs.aws.amazon.com/ses/latest/DeveloperGuide/Welcome.html">Amazon Simple Email Service Developer Guide</a>.</p>'+
-              '</body>'+
-              '</html>',
-    SubjectPart: 'SUBJECT_LINE',
-    TextPart: 'TEXT_CONTENT'
-  }
-};
+AWS.config.loadFromPath(__dirname + '/aws.json');
 
-// Create the promise and SES service object
-var templatePromise = new AWS.SES({apiVersion: '2010-12-01'}).createTemplate(params).promise();
+var templatePromise = new AWS.SES({apiVersion: '2010-12-01'}).getTemplate({TemplateName: 'test1'}).promise();
 
 // Handle promise's fulfilled/rejected states
 templatePromise.then(
   function(data) {
-    console.log(data);
+    console.log(data.SubjectPart);
   }).catch(
     function(err) {
     console.error(err, err.stack);
