0a66494e077995c370b30305c8dd491116a264f8
annubv <annubv25@gmail.com>
2020-02-15 19:12:30 +0530

    added content from backend to sent-campaign page

--- backend/models/sent-campaign.js
+++ backend/models/sent-campaign.js
@@ -1,15 +1,21 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
-const schema = mongoose.Schema
+const schema = mongoose.Schema;
 
-const sentCampaignSchema = new schema({
-    campaign : {type : String ,required : true},
-    campaign_subject : {type : String , required : true},
-    sender : {type: String,required: true},
-    sender_email : {type: String,required: true},
-    reply_email : {type: String,requiredl: true},
-    lists: {type: Array,required: true},
-    total_contacts : {type:Number, required: true}
-},{timestamps : true})
+const sentCampaignSchema = new schema(
+  {
+    campaign: { type: String, required: true },
+    campaign_subject: { type: String, required: true },
+    sender: { type: String, required: true },
+    sender_email: { type: String, required: true },
+    reply_email: { type: String, requiredl: true },
+    lists: { type: Array, required: true },
+    total_contacts: { type: Number, required: true }
+  },
+  { timestamps: true }
+);
 
-module.exports = sent_campaign = mongoose.model("sent_campaign" , sentCampaignSchema);
+module.exports = sent_campaign = mongoose.model(
+  "sent_campaign",
+  sentCampaignSchema
+);
